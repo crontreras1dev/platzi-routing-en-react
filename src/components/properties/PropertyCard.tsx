de35e57eab921edefc35a9fb50a89ff15c3c55f8
crontreras1 <crontreras1@gmail.com>
2026-08-07 10:59:48 -0500

    class 6. programmatic programming in react with usenavigation

--- src/components/properties/PropertyCard.tsx
+++ src/components/properties/PropertyCard.tsx
@@ -1,12 +1,19 @@
 import type { Property } from "../../types/property";
+import { useNavigate } from "react-router";
 
 type PropertyCardProps = {
   property: Property;
 };
 
 export function PropertyCard({ property }: PropertyCardProps) {
+  const navigate = useNavigate();
+  
+  const handlePropertyClick = () => {
+    navigate(`/property/${property.id}`);
+  };
+
   return (
-    <article className="property-card">
+    <article className="property-card" onClick={ handlePropertyClick }>
       <img src={property.image} alt={property.title} />
       <div className="property-card-content">
         <h4>{property.title}</h4>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,18 @@
 import { Header } from "../components/layout/Header";
+import { useNavigate } from "react-router";
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/profile", { replace: true });
+  };
+
   return (
     <>
       <Header />
       <div>LoginPage</div>
+      <button onClick={ handleLogin }>Login</button>
     </>
   );
 };
