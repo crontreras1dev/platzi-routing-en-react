c9f73a49e0eec60a123536bf65ae091a5f30519a
crontreras1 <crontreras1@gmail.com>
2026-08-08 16:35:19 -0500

    class 10. dinamic routes in react. detail page

--- src/pages/PropertyDeatilPage.tsx
+++ src/pages/PropertyDeatilPage.tsx
@@ -1,12 +1,31 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
+import { properties } from "../data/properties";
 
 export const PorpertyDetailPage = () => {
   const { id } = useParams();
+  const property = properties.find(item => item.id === Number(id))
 
   return (
-    <main className="main-content">
-      <h1>Detalle de propiedad</h1>
-      <p>Estamos viendo la propiedad id: { id }</p>
+    <main className="main-content property-detail">
+      <div className="property-detail-image">
+        <img src={ property?.image } alt={ property?.title } />
+
+        <span>{ property?.type }</span>
+      </div>
+
+      <section className="property-detail-content">
+        <p className="eyebrow">Alojamiento Destacado</p>
+
+        <h1>{ property?.title }</h1>
+
+        <p className="property-detail-location">{ property?.location }</p>
+
+        <div className="property-detail-booking">
+          <strong>${ property?.price } / Noche</strong>
+
+          <Link to={`/booking/${property?.id}`}>Reserva Ahora</Link>
+        </div>
+      </section>
     </main>
   );
 };
