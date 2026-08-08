7f9738ce892a6a6c8c0ab99e6f556c39e8cbe2af
crontreras1 <crontreras1@gmail.com>
2026-08-08 17:54:17 -0500

    class 12. query params for search in react

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,31 @@
+import { useSearchParams } from "react-router";
+import { PropertyList } from "../components/properties/PropertyList";
+import { properties } from "../data/properties";
+
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const destination = searchParams.get("destinantion") || "";
+  const type = searchParams.get("type") || "";
+
+  const filteredProperties = properties.filter(property => {
+    const matchesDestination = property.location.toLowerCase().includes(destination.toLocaleLowerCase());
+    const matchesType = type ? property.type === type: true;
+
+    return matchesDestination && matchesType;
+  });
+
   return (
-    <h1>Search Page</h1>
+    <>
+      <main className="main-content search">
+        <h1>Resultados de busqueda</h1>
+
+        <p>Destiono: { destination || "Todos" }</p>
+
+        <p>Tipo: { type || "Todos" }</p>
+
+        <PropertyList properties={ filteredProperties }/>
+      </main>
+    </>
   );
 };
